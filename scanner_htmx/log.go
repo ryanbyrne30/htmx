904a732763b8391082a5196458f709230c057c21
ryanbyrne30 <ryanjbyrne30@gmail.com>
2023-09-13 18:35:10 -0700

    Server uses custom error logger

--- scanner_htmx/log.go
+++ scanner_htmx/log.go
@@ -5,5 +5,8 @@ import (
 	"os"
 )
 
-var InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-var ErrorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
+// Custom leveled info logger
+var infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+// Custom leveled error logger
+var errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
--- scanner_htmx/main.go
+++ scanner_htmx/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,17 +11,29 @@ import (
 
 type config struct {
 	publicDir string
+	port int
 }
 
 func main() {
 	var cfg config
 
 	flag.StringVar(&cfg.publicDir, "public", "./static", "the directory to server static files from. Defaults to ./static")
+	flag.IntVar(&cfg.port, "port", 8080, "port to run the server on. Default 8080")
 	flag.Parse()
 
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(cfg.publicDir))))
 
-	router.Init(r, InfoLog, ErrorLog)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	router.Init(r, infoLog, errorLog)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%v", cfg.port),
+		ErrorLog: errorLog,
+		Handler: r,
+	}
+
+	infoLog.Printf("Starting server on port %v", cfg.port)
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
