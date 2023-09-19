ac744c8c84500a5eee88fd6efb43ad854131b2b2
ryanbyrne30 <ryanjbyrne30@gmail.com>
2023-09-19 12:59:14 -0700

    Added panic recovery

--- scanner_htmx/cmd/web/middleware.go
+++ scanner_htmx/cmd/web/middleware.go
@@ -1,10 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) logMw(next http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-		app.infoLog.Printf("%s %s", r.Method, r.URL.Path)
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		next.ServeHTTP(w, r)
 	})
 }
@@ -16,7 +19,20 @@ func (app *application) secureHeadersMw(next http.Handler) http.Handler {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
-		
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanicMw(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		defer func () {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
