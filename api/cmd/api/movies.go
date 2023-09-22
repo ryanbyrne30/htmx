abd5c3978ea63e03c53033af9afe686930a5569c
ryanbyrne30 <ryanjbyrne30@gmail.com>
2023-09-22 07:31:02 -0700

    Added json writer helper

--- api/cmd/api/movies.go
+++ api/cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/ryanbyrne30/htmx/api/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,11 +15,25 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
-
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show movie %d", id)
+	movie := &data.Movie{
+		ID: 1,
+		CreatedAt: time.Now(),
+		Title: "My Movie",
+		Runtime: 102,
+		Year: 2023,
+		Genres: []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "There was an issue with your request", http.StatusInternalServerError)
+	}
 }
--- api/cmd/api/routes.go
+++ api/cmd/api/routes.go
@@ -8,6 +8,7 @@ import (
 
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
+	router.RedirectTrailingSlash = true
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
